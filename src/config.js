47c416a87ff90775f071395a707fb32d01e49697
bendevera <ben10devera@gmail.com>
2020-03-16 21:16:48 -0600

    Preparing for heroku

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
-// const Config = {
-//     apiURL: "https://writ-api.herokuapp.com"
-// }
-
 const Config = {
-    apiURL: "http://localhost:5000"
+    apiURL: "https://writ-api.herokuapp.com"
 }
+
+// const Config = {
+//     apiURL: "http://localhost:5000"
+// }
 
 export default Config;
